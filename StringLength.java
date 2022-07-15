685d52c545070dcab0b834f920611c7afe1cae43
vinoda2 <109263358+vinoda2@users.noreply.github.com>
2022-07-15 11:52:10 +0530

    Add files via upload

---
+++ StringLength.java
@@ -0,0 +1,12 @@
+package com.xworkz.practice;
+
+public class StringLength {
+	static String s1="Vinoda";
+	static int l=s1.length();
+	
+	public static void main(String args[]) {
+	
+		System.out.println(l);
+	}
+
+}
